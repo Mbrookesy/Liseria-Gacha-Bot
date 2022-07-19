8373fcfdbe9584dd03b15f6a84136546d610b3a5
Mbrookesy <milobrookes0@gmail.com>
2022-07-19 12:52:30 +0100

    Added dummy data database for OCs

--- config/database_setup.py
+++ config/database_setup.py
@@ -1,13 +1,48 @@
 import sqlite3
+import csv
 
 
 def database_setup():
-    conn = sqlite3.connect("gachaDatabase.db")
-    print("Opened database successfully")
+    try:
+        conn = sqlite3.connect("gachaDatabase.db")
+        cursor = conn.cursor()
 
-    conn.execute("""CREATE TABLE IF NOT EXISTS USER
-    (ID INT PRIMARY KEY NOT NULL,
-    GOLD INT            NOT NULL);
-    """)
+        with open("config/ocs.csv", mode="r") as file:
+            data = csv.reader(file)
 
-    conn.close()
+            print("Opened database successfully")
+
+            conn.execute("""CREATE TABLE IF NOT EXISTS USER
+            (ID INT PRIMARY KEY NOT NULL,
+            GOLD INT            NOT NULL);
+            """)
+
+            conn.execute("""CREATE TABLE IF NOT EXISTS OCS
+            (ID         INT PRIMARY KEY NOT NULL,
+            NAME        VARCHAR(255)    NOT NULL,
+            PRICE       INT             NOT NULL,
+            RARITY      INT             NOT NULL,
+            DESCRIPTION VARCHAR(255));
+            """)
+
+            cursor.execute(f"""SELECT ID FROM OCS WHERE ID = 1;""")
+            first_cell = cursor.fetchall()
+
+            if not first_cell:
+                insert_oc_query = """
+                INSERT INTO OCS (id, name, price, rarity, description)
+                VALUES (?, ?, ?, ?, ?);"""
+
+                print("Inserted oc data into database")
+
+                cursor.executemany(insert_oc_query, data)
+                conn.commit()
+
+            cursor.close()
+            conn.close()
+    #Todo: Make error handling more specific
+    except:
+        print("Unknown Database Error")
+
+
+
